b307146a2f3ad21bd366d6209a5fbaedbd214307
AbderrahmaneRaquibi <a.raquibi4554@uca.ac.ma>
2024-12-26 16:48:11 +0100

    Describe your changes here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Navbar from "./components/navbar";
 
 
 export default function Home() {
   return (
     <div className="">
-        <h1>FIRST STEP</h1>
+        <Navbar/>
     </div>
   );
 }
