e7cf531bb67ba627b6c6978861c434d7fbbc1c44
ABDERRAHMANE_RAQUIBI <a.raquibi4554@uca.ac.ma>
2025-06-08 17:49:53 +0100

    Deleted page.tsx

--- src/app/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { SignUp } from '@clerk/nextjs';
-export default function Home() {
-    return (
-        <div className="flex justify-center items-center">
-            <SignUp forceRedirectUrl={'/'} />
-        </div>
-    );
-}
