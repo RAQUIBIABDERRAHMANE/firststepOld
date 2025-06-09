e53c7399483cd20ab679cc50ead2b45e0a0092cd
ABDERRAHMANE_RAQUIBI <a.raquibi4554@uca.ac.ma>
2025-06-09 11:08:46 +0100

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ClerkProvider } from '@clerk/nextjs';
 import Head from "next/head";
 import "./globals.css";
 
@@ -47,7 +46,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <Head>
           <link rel="shortcut icon\" href="/favicon.ico\" type="image/x-icon" />
@@ -80,6 +78,5 @@ export default function RootLayout({
           />
         </body>
       </html>
-    </ClerkProvider>
   );
 }
